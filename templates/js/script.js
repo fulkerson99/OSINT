e59127945b9b887b547acea679c8687849978e12
Julio <j.a.pagan.q@gmail.com>
2020-12-01 00:30:46 -0400

    input stored in variable to use in function

--- templates/js/script.js
+++ templates/js/script.js
@@ -1,16 +1,31 @@
-function generateReport(){
-    alert("Button is working");
+function igGenerateReport(){
+    var igUser = document.getElementById("igUser").value;
+    alert("Scanning " + igUser);
 }
 
-function collectData(){
+function igCollectData(){
+    alert("This button is also working");
+}
+
+
+
+
+function twGenerateReport(){
+    var twUser = document.getElementById("twUser").value;
+    alert("Scanning " + twUser);
+}
+
+function twCollectData(){
     alert("This button is also working");
 }
 
 function loadHandlers(){
     //sec1
-    document.getElementById("generateReport").addEventListener("click", generateReport);
-    //sec2
-    document.getElementById("collectData").addEventListener("click", collectData);
+    document.getElementById("IGgenerateReport").addEventListener("click", igGenerateReport);
+    document.getElementById("IGcollectData").addEventListener("click", igCollectData);
+
+    document.getElementById("TWgenerateReport").addEventListener("click", twGenerateReport);
+    document.getElementById("TWcollectData").addEventListener("click", twCollectData);
 }
 
 window.addEventListener("load", loadHandlers);
