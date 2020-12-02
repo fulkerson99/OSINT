fc6c4958bce82013a6f86d371143971f6fb779d2
Naveed Hussain <nhussain2@hawk.iit.edu>
2020-12-02 10:12:10 +0500

    add get post methods and instagram intelligence dummy pg

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def main():
     return render_template("main.html")
+
+@app.route('/instagram_intelligence_report' , methods=['GET','POST'])
+def instagram_intelligence_report():
+    return render_template("instagram_intelligence.html")
+
+
 
 if __name__ == '__main__':
     app.run()
