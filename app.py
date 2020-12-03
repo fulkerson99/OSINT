56675c73200011ae2a778f022c9a8220169864ee
Naveed Hussain <nhussain2@hawk.iit.edu>
2020-12-03 13:16:26 +0500

    fixed instagram

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import random
 import string
 import sys
 import time
+import html
 
-
+import twint
+from igramscraper.instagram import Instagram
+from twitter_scraper import Profile
 
 
 app = Flask(__name__)
@@ -21,71 +24,35 @@ def main():
 @app.route('/instagram_intelligence_report' , methods=['GET','POST'])
 def instagram_intelligence_report():
     iguser = request.form['iguser']
-    if iguser:
+    instagram = Instagram()
 
-        username = iguser
-        useragents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+    instagram.with_credentials('testingOSINT', 'osintpass123')
+    instagram.login()
 
-        # scrape profile
+    account = instagram.get_account(iguser)
 
-        # Get the html data with the requests module
-        r = requests.get(f'http://instagram.com/{username}', headers={'User-Agent': random.choice(useragents)})
-
-        soup = BeautifulSoup(r.text, 'html.parser')
-
-        # Find the tags that hold the data we want to parse
-        general_data = soup.find_all('meta', attrs={'property': 'og:description'})
-        more_data = soup.find_all('script', attrs={'type': 'text/javascript'})
-        description = soup.find('script', attrs={'type': 'application/ld+json'})
-        # Try to parse the content -- if it fails then the program exits
-        try:
-            text = general_data[0].get('content').split()
-            description = json.loads(description.get_text())
-            profile_meta = json.loads(more_data[3].get_text()[21:].strip(';'))
-            # Works!
-        except:
-            # ADD TEMPLATE HERE
-            return render_template("error.html")
-        
-        profile_name = profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['username']
-        profile_url = description['mainEntityofPage']['@id']
-        profile_follower = text[0]
-        profile_following = text[2]
-        profile_posts = text[4]
-        profile_bio = str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['biography'])
-        profile_pic_url= str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url_hd'])
-        profile_is_business_account = str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_business_account'])
-        profile_connected_to_fb = str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['connected_fb_page'])
-        profile_externalurl = str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['external_url'])
-        profile_joined_recently = str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                                        'is_joined_recently'])
-        profile_business_category_name = str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
-                                     'business_category_name'])
-        profile_is_private = str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_private'])
-        profile_is_verified=str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_verified'])        
-        
-
-        return render_template("instagram_intelligence.html", iguser=iguser, username=username, profile_name=profile_name, profile_url=profile_url, profile_follower=profile_follower, profile_following=profile_following, profile_posts=profile_posts, profile_bio=profile_bio, profile_pic_url=profile_pic_url, profile_is_business_account=profile_is_business_account, profile_connected_to_fb=profile_connected_to_fb,profile_externalurl=profile_externalurl, profile_joined_recently=profile_joined_recently,  profile_business_category_name= profile_business_category_name, profile_is_private=profile_is_private,   profile_is_verified=profile_is_verified)
-
-    else:
-        return 'No user entered!'
-
+    username = account.username
+    profile_name = account.full_name
+    profile_bio = account.biography
+    profile_pic_url = account.get_profile_picture_url()
+    profile_externalurl = account.external_url
+    profile_posts = account.media_count
+    profile_follower = account.followed_by_count
+    profile_following = account.follows_count
+    profile_is_private = account.is_private
+    profile_is_verified = account.is_verified
+    
+    return render_template("instagram_intelligence.html", iguser=iguser, username=username, profile_name=profile_name, profile_follower=profile_follower, profile_following=profile_following, profile_posts=profile_posts, profile_bio=profile_bio, profile_pic_url=profile_pic_url, profile_externalurl=profile_externalurl,  profile_is_private=profile_is_private, profile_is_verified=profile_is_verified)
+      
 
 @app.route('/twitter_report' , methods=['GET','POST'])
 def twitter_report():
     username = request.form['twitteruser']
     if username:
-        return "hello"
+        profile = Profile(f"{username}")
+        return print(profile.to_dict())
+        
+        
     else:
         return render_template("error.html")
 
